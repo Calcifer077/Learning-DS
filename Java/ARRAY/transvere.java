6de36e209f9a2105700628969209142d9375d0d6
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-07 18:30:34 +0530

    Update and rename Java/classwork.java to Java/ARRAY/transvere.java

---
+++ Java/ARRAY/transvere.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Helloworld{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int arr_size = 0;
+        if(in.hasNextInt()){
+            arr_size = in.nextInt();
+        }
+        
+        int[] arr = new int[arr_size];
+        
+        System.out.println("Enter the elements for your array: ");
+        for(int i = 0; i < arr_size; i++){
+            if(in.hasNextInt()){
+                arr[i] = in.nextInt();
+            }
+        }
+        System.out.println("The elements of the array is: ");
+        for(int i = 0; i < arr_size; i++){
+            System.out.println(arr[i] + " ");
+        }
+        in.close();
+    }
+}
