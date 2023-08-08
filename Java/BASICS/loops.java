00333159c3852b6419a2439daa7152c93e369fca
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-08 19:31:26 +0530

    Create loops.java

---
+++ Java/BASICS/loops.java
@@ -0,0 +1,11 @@
+//Program to print "Hello world 10 times.
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Program to print Hello world 10 times. \r"); 
+        for(int i = 0; i< 10;i++){
+            System.out.println("Hello world.\r");
+        }
+    }
+}
