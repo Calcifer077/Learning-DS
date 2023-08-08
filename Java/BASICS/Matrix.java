4f08a38b68a7d1004856b79f98e154a051f421ad
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-08 20:01:46 +0530

    Create Matrix.java

---
+++ Java/BASICS/Matrix.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows and columns for your array: ");
+        int r = in.nextInt();
+        int c = in.nextInt();
+        
+        int[][] arr = new int[10][10];
+        System.out.println("Enter your array: ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                System.out.println(arr[i][j]);
+            }
+            System.out.println("\r");
+        }
+    }
+}
