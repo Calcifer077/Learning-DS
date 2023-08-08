1b1649bf218c196d6e4ecce829660897595cb4b2
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-08 19:38:51 +0530

    Update loops.java

--- Java/BASICS/loops.java
+++ Java/BASICS/loops.java
@@ -9,3 +9,16 @@ public class Main{
         }
     }
 }
+//Program to sum first 10 integers.
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Program to sum first 10 integers \r"); 
+        int sum = 0;
+        for(int i = 0; i< 10;i++){
+            sum =+ sum + i;
+        }
+        System.out.println(sum);
+    }
+}
