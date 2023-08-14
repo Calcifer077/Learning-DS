056d222cbbe06cdae767872adbb3406a2447e435
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-14 21:45:22 +0530

    Update Searching.java

--- Java/ARRAY/Searching.java
+++ Java/ARRAY/Searching.java
@@ -24,3 +24,42 @@ public class Main{
         }
     }
 }
+..................................................................
+..................................................................
+..................................................................
+import java.util.Scanner;
+
+public class Main{
+    static int binarysearch(int arr[], int size, int key){
+        int low, mid, end;
+        low = 0;
+        end = size - 1;
+        while(low <= end){
+            mid = (low + end)/2;
+            if(arr[mid] == key){
+                return mid;
+            }
+            else if (arr[mid] < key){
+                low = mid + 1;
+            } 
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+        int size = arr.length;
+        System.out.println("Enter the element you want to search: ");
+        int key = in.nextInt();
+        int flag = binarysearch(arr, size, key);
+        if(flag == -1){
+            System.out.println("Element not found");
+        }
+        else{
+            System.out.println("Element found at: " + (flag + 1));
+        }
+    }
+}
