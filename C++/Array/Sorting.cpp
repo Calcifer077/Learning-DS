cb1d1dbecd5b58440248e2dc404b6a324bb5a5c7
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-15 16:05:59 +0530

    Update Sorting.cpp

--- C++/Array/Sorting.cpp
+++ C++/Array/Sorting.cpp
@@ -22,3 +22,26 @@ int main(){
 	 
 	return 0;
 }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[] = {23, 12, 99, 7, 54, 65};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int counter = 1;
+	while(counter < size){
+		for(int i = 0; i < size -1;i++){
+			if(arr[i] > arr[i+1]){
+				int temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+			}
+		}
+		counter++;
+	}
+	for(int i = 0; i < size; i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
