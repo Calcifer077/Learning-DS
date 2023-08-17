f43d37c0118dc11b1c8845e23de588fca8ea6e4f
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-17 18:35:40 +0530

    Update Basics.cpp

--- C++/Linked List/Basics.cpp
+++ C++/Linked List/Basics.cpp
@@ -1,10 +1,11 @@
-
 #include<iostream>
 using namespace std;
 
-class node{
+class node{//node is the pointer that points to the next location.
 	public:
-		int data;
+	//A node is the basic structure of the linked list. It stores data as value than pointer.
+	//The pointer points out the address of the next value.
+		int data;//It is the value stored in the Linked list.
 		node* next;
 		
 		node(int val){
@@ -12,15 +13,16 @@ class node{
 			next = NULL;
 		}
 };
-
-void insertAthead(node* &head, int val){
+//The last pointer of a linked list is NULL.
+//The first node is known as head.
+void insertAthead(node* &head, int val){//It will add the value at the first position.
 	node* n = new node(val);
 	n->next = head;
 	head = n;
 }
 
-void insertAttail(node* &head, int val){
-	node* n = new node(val);
+void insertAttail(node* &head, int val){//It will add the value at the last position.
+	node* n = new node(val)
 	
 	if(head == NULL){
 		head = n;
