1c5b9eac38af654ce08538e2632ae56627da34b5
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-21 13:55:31 +0530

    Create Basics.java

---
+++ Java/CLASSES AND OBJECTS/INHERITANCE/Basics.java
@@ -0,0 +1,18 @@
+class Base{
+    Base(){
+        System.out.println("Base class constructor called!");
+    }
+}
+
+class Derived extends Base{
+    Derived(){
+        System.out.println("Derived class constructor called!");
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Base obj1 = new Base();
+        Derived obj2 =  new Derived();//will also print the output of base class because it has derived it.
+    }
+}
