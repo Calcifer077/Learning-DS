8030d5e63c43b0032faf16d4b3a1622e5c457cfe
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-21 14:20:24 +0530

    Create Method_Overloading.cpp

---
+++ Java/CLASSES AND OBJECTS/POLYMORPHISM/Method_Overloading.cpp
@@ -0,0 +1,22 @@
+//Polymorphism
+import java.util.Scanner;
+class Base{
+    void area(){
+        System.out.println("No arguments passed");
+    }
+    void area(int a, int b){
+        System.out.println("The area of the gieven rectangle is: " + (a * b));
+    }
+    void area(double a, double b){
+        System.out.println("The area of the given rectangle is: " + (a + b));
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Base obj = new Base();
+        obj.area();
+        obj.area(2,3);
+        obj.area(5.0, 4.8);
+    }
+}
