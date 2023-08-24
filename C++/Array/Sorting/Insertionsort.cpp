1122d0e235bef793307f6623a7cb2b8ff852c205
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2023-08-24 19:41:57 +0530

    Update Insertionsort.cpp

--- C++/Array/Sorting/Insertionsort.cpp
+++ C++/Array/Sorting/Insertionsort.cpp
@@ -1,6 +1,7 @@
 //Insertion Sort->
 //Insert an element from unsorted array to its correct position in sorted array.
 //Start from Index[1] leaving Index[0] aside.
+//Imagine sorting a deck of cards.
 #include<iostream>
 using namespace std;
 
@@ -8,10 +9,11 @@ int main(){
 	int arr[] = {23, 12, 99, 7, 54, 65};
 	int size = sizeof(arr) / sizeof(arr[0]);
 	
-	for(int i = 1; i < size; i++){
-		int current = arr[i];
+	for(int i = 1; i < size; i++){//This loop will select an element which will be needed to sorted.
+		int current = arr[i];//That element is stored here.
 		int j = i - 1;
-		while(arr[j] > current && j >= 0){
+		while(arr[j] > current && j >= 0){//This loop will compare 'current' element to all the element on its left and insert
+						  //it to its right place.
 			arr[j+1] = arr[j];
 			j--;
 		}
