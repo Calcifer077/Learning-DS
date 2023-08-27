9aa7c03e477e1c9cf5fe44bd9eb3202ce5c38650
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2023-08-27 18:03:47 +0530

    Update Interface.java

--- Java/CLASSES AND OBJECTS/ABSTRACTION/Interface.java
+++ Java/CLASSES AND OBJECTS/ABSTRACTION/Interface.java
@@ -23,3 +23,38 @@ class Main{
         obj.sleep();
     }
 }
+.........................................................................................
+.........................................................................................
+.........................................................................................
+...........................>>>>>>>>.MULTIPLE INTERFACE.<<<<<<<<..........................
+interface Animal{
+    public void animalSound();
+    public void sleep();
+}
+
+interface Mammal{
+    public void display();
+}
+class Dog implements Animal, Mammal{
+    public void animalSound(){
+        System.out.println("The Dog says Bark!!");
+    }
+    public void sleep(){
+        System.out.println("The Dog sleeps!!");
+    }
+    public void display(){
+        System.out.println("This is a method from Mammal interface!!");
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Dog obj = new Dog();
+        obj.animalSound();
+        obj.sleep();
+        obj.display();
+    }
+}
+.........................................................................................
+.........................................................................................
+.........................................................................................
