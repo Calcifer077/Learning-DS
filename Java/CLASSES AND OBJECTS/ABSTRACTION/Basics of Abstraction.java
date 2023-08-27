9354b8ce9c2265d976b190f745d5a01c319d7805
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2023-08-27 16:17:27 +0530

    Update and rename Basics.java to Basics of Abstraction.java

---
+++ Java/CLASSES AND OBJECTS/ABSTRACTION/Basics of Abstraction.java
@@ -0,0 +1,23 @@
+//Abstraction is the hiding of certain details and only showing the details which are necessary.
+//Example -> Working of a car. We don't knoq about each and every details of the car but we know the end results which is its motion.
+abstract class Animal{//declaring abstract class which is neccassart to acieve abstraction.
+    public abstract void animalSound();//abstract method -> doesn't have body in the parent class but will have in the subclass.
+    
+    public void sleep(){//normal method with its body.
+        System.out.println("Sleeping!!!!!!");
+    }
+}
+
+class Dog extends Animal{
+    public void animalSound(){//Defining the abstract method here.
+        System.out.println("The Dog barks!!");
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Dog obj = new Dog();
+        obj.animalSound();
+        obj.sleep();
+    }
+}
