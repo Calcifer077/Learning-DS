412e0ee02b86f1f962ba6c1f983e1326b8848ba3
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-08-30 18:21:33 +0530

    Create Reverse K node.cpp

---
+++ C++/Linked List/Reverse K node.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void InsertAtTail(node* &head, int val){
+    node* n = new node(val);
+    
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+node* reverse(node* &head, int k){
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextptr;
+    
+    int count = 0;
+    while(currptr != NULL && count < k){
+        nextptr = currptr -> next;
+        currptr -> next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    
+    if(nextptr != NULL){
+        head->next = reverse(nextptr, k);
+    }
+    
+    return prevptr;
+}
+int main(){
+    node* head = NULL;
+    InsertAtTail(head, 1);
+    InsertAtTail(head, 2);
+    InsertAtTail(head, 3);
+    InsertAtTail(head, 4);
+    InsertAtTail(head, 5);
+    InsertAtTail(head, 6);
+    InsertAtTail(head, 7);
+    display(head);
+    
+    int k = 2;
+    node* newhead = reverse(head, k);
+    display(newhead);
+    return 0;
+}
