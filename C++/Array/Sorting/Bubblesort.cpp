03f90abcf01dc3db2a20559f109165fb668024e9
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-09-05 18:08:38 +0530

    Update Bubblesort.cpp

--- C++/Array/Sorting/Bubblesort.cpp
+++ C++/Array/Sorting/Bubblesort.cpp
@@ -2,6 +2,9 @@
 //According to this algorithm we repeatedly swap two adjacent elements if they are in wrong order. Meaning if the element that comes first
 //	is larger than the element that comes later on.
 //	THis algo runs for " size - 1 " iterations before we get our sorted array.	
+//This sorting is a stable sorting technique which means that the elements which have the same value will also have the same value in the final result.
+//Time complexity -> O(n^2).
+//Space complexity -> O(1). 
 #include<iostream>
 using namespace std;
 
