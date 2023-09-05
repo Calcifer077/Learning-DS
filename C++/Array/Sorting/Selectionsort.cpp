bc2d874c03ba21ebda66ac01d0ffff72e5437079
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-09-05 18:26:40 +0530

    Update Selectionsort.cpp

--- C++/Array/Sorting/Selectionsort.cpp
+++ C++/Array/Sorting/Selectionsort.cpp
@@ -1,6 +1,22 @@
 ...............................SELECTION SORT............................
 //The below given program is of selection sort.
 //It says that find the minimum element in your unsoerted array and swap it with element at begining.
+//Time complexity -> One loop to to select an element in the array -> O(n).
+//		     Another loop for comparison -> O(n).
+//		     O(n) * O(n) = O(n^2).
+
+//Space complexity -> O(1).
+
+/*
+Advantages of Selection sort ->
+	1. Simple and easy to understand.
+ 	2. Works well on small datasets.
+
+Disadvantages of Selection sort ->
+	1. It takes a time of O(n^2) in both best cases and worst case.
+ 	2. Does not work well on large datasets.
+  	3. It is not stable.
+*/
 #include<iostream>
 using namespace std;
 
