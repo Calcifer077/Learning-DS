9198b3444c88f9fc8c754e2d05bfa72ba48aa73e
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-09-17 19:24:45 +0530

    Update Merge.cpp

--- C++/Linked List/Doublelinklist/Merge.cpp
+++ C++/Linked List/Doublelinklist/Merge.cpp
@@ -52,21 +52,22 @@ void display(node* head){
 }
 
 node* merge(node* &head1, node* &head2){
-    node* p1 = head1;
-    node* p2 = head2;
+    //Declaring two pointers.
+    node* p1 = head1;//Points to the head of first list.
+    node* p2 = head2;//Points to the head of second list.
     node* dummynode = new node(-1);
-    node* p3 = dummynode;
+    node* p3 = dummynode;//Points to the resultant linked list.
     
     while(p1 != NULL && p2 != NULL){
         if(p1->data < p2->data){
-            p3->next = p1;
+            p3->next = p1;//Adding data to the next.
             p1 = p1->next;
         }
         else{
             p3->next = p2;
             p2 = p2->next;
         }
-        p3 = p3->next;
+        p3 = p3->next;//Iterating p3.
     }
     
     while(p1 != NULL){
@@ -80,7 +81,7 @@ node* merge(node* &head1, node* &head2){
         p3 = p3->next;
     }
     
-    return dummynode->next;
+    return dummynode->next;//Doing next because the first value is -1 declared above.
 }
 int main(){
     node* head1 = NULL;
@@ -157,16 +158,16 @@ void display(node* head){
 }
 
 node* merge(node* &head1, node* &head2){
-    if(head1 == NULL){
+    if(head1 == NULL){//If the first linked list is empty it will simply return the second list in the answer.
         return head2;
     }
-    if(head2 == NULL){
+    if(head2 == NULL){//If the second linked list is empty it will simply return the first list in the answer.
         return head1;
     }
-    node* result;
-    if(head1->data < head2->data){
+    node* result;//Declaring a result pointer.
+    if(head1->data < head2->data){//If data of head1 is less than the data of head2 we will give the node to result.
         result = head1;
-        result->next = merge(head1->next, head2);
+        result->next = merge(head1->next, head2);//Calling the merge function again.
     }
     else{
         result = head2;
