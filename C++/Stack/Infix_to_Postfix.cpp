a297d66a6e0b3538da19804b956e8aa80189fc65
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-09-20 22:37:19 +0530

    Create Infix_to_Postfix.cpp

---
+++ C++/Stack/Infix_to_Postfix.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<stack>
+#include<math.h>
+using namespace std;
+//go from to left to right.
+
+int prec(char c){//Checks precedence of the operators and returns a number accordingly.
+    if(c == '^'){
+        return 3;
+    }
+    else if(c == '*' || c == '/'){
+        return 2;
+    }
+    else if(c == '+' || c == '-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+string infixtopostifix(string s){
+    stack<char> st;
+    string res;
+    
+    for(int i = 0; i < s.length(); i++){
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')){
+            res += s[i];
+        }
+        else if(s[i] == '('){
+            st.push(s[i]);
+        }
+        else if(s[i] == ')'){
+            while(!st.empty() && st.top() != '('){
+                res += st.top();
+                st.pop();
+            }
+            if(!st.empty()){
+                st.pop();
+            }
+        }
+        else{
+            while(!st.empty() && prec(st.top()) > prec(s[i])){
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while(!st.empty()){
+        res += st.top();
+        st.pop();
+    }
+    return res;
+}
+int main(){
+    cout<<infixtopostifix("(a-b/c)*(a/k-l)");
+    return 0;
+}
