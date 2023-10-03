816ce8c97f6d266d8dd66af25e724631af0845e1
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-03 17:50:36 +0530

    Update Basics of queue.cpp

--- C++/Queue/Basics of queue.cpp
+++ C++/Queue/Basics of queue.cpp
@@ -171,6 +171,8 @@ int main(){
 .........................................................................................
 .........................................................................................
 .........................................................................................
+//Using stacks
+    //Method I
 #include"bits/stdc++.h"
 using namespace std;
 
@@ -214,6 +216,57 @@ int main(){
     q.push(4);
     cout<<q.pop()<<endl;
     q.push(5);
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    cout<<q.pop()<<endl;
+    return 0;
+}
+.........................................................................................
+.........................................................................................
+.........................................................................................
+    //Method II
+#include"bits/stdc++.h"
+using namespace std;
+
+class Queue{
+    stack<int> s1;
+    
+    public:
+        void push(int x){//O(1)
+            s1.push(x);
+        }
+        int pop(){//O(n)
+            if(s1.empty()){
+                cout<<"Queue is empty."<<endl;
+                return -1;
+            }
+            int x = s1.top();
+            s1.pop();
+            if(s1.empty()){
+                return x;
+            }
+            int item = pop();
+            s1.push(x);
+            return item;
+        }
+        
+        bool empty(){
+            if(s1.empty() ){
+                return true;
+            }
+            return false;
+        }
+};
+int main(){
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    cout<<q.pop()<<endl;
+    q.push(5);
+    cout<<q.pop()<<endl;
     cout<<q.pop()<<endl;
     cout<<q.pop()<<endl;
     cout<<q.pop()<<endl;
