ca603689039ae4cc15d9fdefc72553e0b1551a7e
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-03 22:08:49 +0530

    Update Threading.java

--- Java/MULTI THREADING/Threading.java
+++ Java/MULTI THREADING/Threading.java
@@ -8,3 +8,17 @@ class Main extends Thread{
         t1.start();
     }
 }
+.........................................................................................
+.........................................................................................
+.........................................................................................
+class Main implements Runnable{
+    public void run(){
+        System.out.println("Thread is running!!");
+    }
+    
+    public static void main(String[] args){
+        Main m1 = new Main();
+        Thread t1 = new Thread(m1);
+        t1.start();
+    }
+}
