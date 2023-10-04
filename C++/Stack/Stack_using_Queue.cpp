205a20e07d6b912e7a5cb2a5b47a765d491c8aff
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-04 20:11:26 +0530

    Update Stack_using_Queue.cpp

--- C++/Stack/Stack_using_Queue.cpp
+++ C++/Stack/Stack_using_Queue.cpp
@@ -1,32 +1,31 @@
+    ........METHOD I...........
 #include<bits/stdc++.h>
 using namespace std;
 //Making push method costly.
 
 class Stack{
-    int N;
-    queue<int> q1;
+    int N;//Size of stack
+    queue<int> q1;//It is the resultant Queue.
     queue<int> q2;
     
     public:
     Stack(){
         N = 0;
     }
-    void push(int val){
+    void Push(int val){
         q2.push(val);
         N++;
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        queue<int> temp = q1;
-        q1 = q2;
-        q2 = temp;
+        swap(q1, q2);
     }
-    void pop(){
+    void Pop(){
         q1.pop();
         N--;
     }
-    int top(){
+    int Top(){
         return q1.front();
     }
     int size(){
@@ -35,17 +34,17 @@ class Stack{
 };
 int main(){
     Stack st;
-    st.push(1);
-    st.push(2);
-    st.push(3);
-    st.push(4);
+    st.Push(1);
+    st.Push(2);
+    st.Push(3);
+    st.Push(4);
     
-    cout<<st.top()<<endl;
-    st.pop();
+    cout<<st.Top()<<endl;
+    st.Pop();
     
-    cout<<st.top()<<endl;
+    cout<<st.Top()<<endl;
     
-    cout<<st.size();
+    cout<<st.size()<<endl;
     return 0;
 }
 .........................................................................................
