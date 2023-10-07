e7b13b980ddbadb43ffb4ad9b36c92e77cdcd09a
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-07 22:18:33 +0530

    Update Counting frequency.cpp

--- C++/Array/Anonymus/Counting frequency.cpp
+++ C++/Array/Anonymus/Counting frequency.cpp
@@ -22,24 +22,3 @@ int main(){
 .........................................................................................
 .........................................................................................
 	...............NOT COMPLETED............
-#include<iostream>
-using namespace std;
-
-int power(int x, unsigned int n){
-    int temp;
-    if(n==0){
-        return 1;
-    }
-    temp = power(x, n/2);
-    if(n%2 == 0){
-        return temp * temp;
-    }
-    else{
-        return x * temp * temp;
-    }
-}
-int main(){
-    cout<<power(3,0);
-    
-    return 0;
-}
