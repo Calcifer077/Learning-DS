02570062d5b3acdd9e7f560881398d5caf695354
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-08 18:35:53 +0530

    Create Pipe.java

---
+++ Java/MULTI THREADING/Pipe.java
@@ -0,0 +1,43 @@
+import java.io.PipedReader;//Importing this to read from a pipe
+import java.io.PipedWriter;//Importing this to write in a pipe
+  
+public class First {  
+    public static void main(String[] args) {  
+        try {  
+  
+            final PipedReader read = new PipedReader();//Creating a instance for Pipedreader  
+            final PipedWriter write = new PipedWriter(read);//Creating a instance for pipedWriter and this is connected to the Pipedreader
+  
+            Thread readerThread = new Thread(new Runnable() {//Creating thread for reader 
+                                                             //Here new Runnable is used to create an instance of Runnable interface which is
+                                                             //required to implement thread.
+                public void run() {  //Providing implementation of run method which is neccessary for using threads.
+                    try {  
+                        int data = read.read();  
+                        while (data != -1) {  
+                            System.out.print((char) data);  
+                            data = read.read();  
+                        }  
+                    } catch (Exception ex) {  
+                    }  
+                }  
+            });  
+  
+            Thread writerThread = new Thread(new Runnable() {//Creating thread for Writer  
+                public void run() {  
+                    try {  
+                        write.write("I love educative widgets\n".toCharArray());  //This will be written on the console and will be read by the PipeReader class.
+                    } catch (Exception ex) {  
+                    }  
+                }  
+            });  
+            //Both threads are started here.
+            readerThread.start();  
+            writerThread.start();  
+  
+        } catch (Exception ex) {  
+            System.out.println(ex.getMessage());  
+        }  
+  
+    }  
+}  
