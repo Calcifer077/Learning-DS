d9f6bc2512aa64775900741bfd9fcda3e5313056
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-18 18:57:36 +0530

    Update Height_of_binary_tree.cpp

--- C++/Tree/Binary Tree/Height_of_binary_tree.cpp
+++ C++/Tree/Binary Tree/Height_of_binary_tree.cpp
@@ -36,3 +36,59 @@ int main(){
     cout<<calcHeight(root);
     return 0;
 }
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int calcHeight(Node* root){//O(N)
+    if(root == NULL){
+        return 0;
+    }
+    int lheight = calcHeight(root->left);
+    int rheight = calcHeight(root->right);
+    
+    return max(lheight, rheight) + 1;
+}
+
+int calcdiameter(Node* root){//O(n^2)
+    if(root == NULL){
+        return 0;
+    }
+    int lheight = calcHeight(root->left);
+    int rheight = calcHeight(root->right);
+    
+    int currdiameter = lheight + rheight + 1;
+    
+    int ldiameter = calcdiameter(root->left);
+    int rdiameter = calcdiameter(root->right);
+    
+    return max(currdiameter, max(ldiameter, rdiameter));
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    
+    cout<<calcdiameter(root);
+    return 0;
+}
