98b764ba81ca26b75eb8cf0ec23f9a7ac40a9c00
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-29 12:08:13 +0530

    Update Basics.java

--- Java/CLASSES AND OBJECTS/INHERITANCE/Basics.java
+++ Java/CLASSES AND OBJECTS/INHERITANCE/Basics.java
@@ -19,3 +19,36 @@ class Main{
 .........................................................................................
 .........................................................................................
 .........................................................................................
+class Bike{
+	public int gear;
+	public int speed;
+
+	public Bike(int gear, int speed){
+		this.gear = gear;
+		this.speed = speed;
+	}
+
+	public String display(){//Output type of String.
+		return("No. of gears are "+ gear + "\n" + " Speed of Bike is " + speed);
+	}
+}
+class MountainBike extends Bike{
+	public int setheight;
+	public MountainBike(int gear, int speed, int startheight){
+		super(gear, speed);//Used to refer to the parent class object.
+                           //It is necessary when we have a constructor in the base class.
+		setheight = startheight;
+	}
+
+	@Override public String display(){//@Override method is used to indicate that the child class method is overriding its Base class method.
+                                      //It improves the readibilty of the code.
+		return(super.display() + "\n Seat Height is: " + setheight);
+	}
+}
+
+class Main{
+	public static void main(String[] args) {
+		MountainBike obj = new MountainBike(12, 120, 10);
+		System.out.println(obj.display());
+	}
+}
