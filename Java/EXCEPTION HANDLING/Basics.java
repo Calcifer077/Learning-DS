d0e239d31f370ee0948d2b3e922d22d7ee083bdb
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-29 18:42:33 +0530

    Update Basics.java

--- Java/EXCEPTION HANDLING/Basics.java
+++ Java/EXCEPTION HANDLING/Basics.java
@@ -13,25 +13,3 @@ public class Main{
         System.out.println("Outside try and catch block! ");
     }
 }
-
-
-public class Main{
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        int c;
-        try{
-        c = a / b;
-        System.out.println(c);
-        }
-        catch(NullPointerException n){
-            System.out.println("Exception found! ");
-        }
-        finally(){
-            System.out.println("Inside the final block! ");
-        }
-        System.out.println("Outside try and catch block! ");
-        
-        
-    }
-}
