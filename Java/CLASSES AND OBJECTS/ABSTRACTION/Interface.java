9f0d0ced03bfd455eafd598abb59d8848838d86e
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-29 16:41:26 +0530

    Update Interface.java

--- Java/CLASSES AND OBJECTS/ABSTRACTION/Interface.java
+++ Java/CLASSES AND OBJECTS/ABSTRACTION/Interface.java
@@ -58,3 +58,39 @@ class Main{
 .........................................................................................
 .........................................................................................
 .........................................................................................
+interface P1{
+        //We cannot give definition to the interface method.
+	default void show(){//Default method is used to give definition to methods of an interface.
+		System.out.println("Default method of P1");
+	}
+}
+
+interface P2{
+	default void show(){
+		System.out.println("Default methof of P2");
+	}
+}
+class Test implements P1, P2{
+	@Override//Overriding function. tells the compiler we are doing overriding. Can also be used for readability of the code.
+	public void show(){
+		P1.super.show();//'super' keyword to go to the parent class.
+		P2.super.show();
+	}
+
+	public void showofP1(){
+		P1.super.show();
+	}
+	public void showofP2(){
+		P2.super.show();
+	}
+}
+
+class Main{
+	public static void main(String[] args) {
+		Test obj = new Test();
+		obj.show();
+		System.out.println("Executing specific functions made for P1 and P2");
+		obj.showofP1();
+		obj.showofP2();
+	}
+}
