2352afb2f8a6352801acc2dcdd120b60cf85e9ce
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-31 18:13:46 +0530

    Update check_balanced_height_tree.cpp

--- C++/Tree/Binary Tree/check_balanced_height_tree.cpp
+++ C++/Tree/Binary Tree/check_balanced_height_tree.cpp
@@ -72,3 +72,72 @@ int main(){
     }
     return 0;
 }
+.........................................................................................
+.........................................................................................
+.........................................................................................
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        
+        Node(int val){
+            data = val;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+bool balancedtree(Node* root, int* height){
+    if(root == NULL){
+        return true;
+    }
+    int lh = 0, rh = 0;
+    if(balancedtree(root->left, &lh) == false){
+        return false;
+    }
+    if(balancedtree(root->right, &rh) == false){
+        return false;
+    }
+    *height = max(lh, rh) + 1;
+    if(abs(lh - rh) <= 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    Node* root1 = new Node(1);
+    root1->left = new Node(2);
+    root1->left->left = new Node(3);
+    
+    int height = 0;
+    
+    if(balancedtree(root, &height)){
+        cout<<"balancedtree";
+    }
+    else{
+        cout<<"Not balancedtree";
+    }
+    
+    if(balancedtree(root1, &height)){
+        cout<<"balancedtree";
+    }
+    else{
+        cout<<"Not balancedtree";
+    }
+    return 0;
+}
