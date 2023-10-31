4a0148e6c70c7060647d243483cf13c379816645
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-10-31 18:55:40 +0530

    Update check_balanced_height_tree.cpp

--- C++/Tree/Binary Tree/check_balanced_height_tree.cpp
+++ C++/Tree/Binary Tree/check_balanced_height_tree.cpp
@@ -1,3 +1,7 @@
+//Balanced Height tree --> For each node, the difference between the heights of the left subtree and right subtree should be leass than or equal to 1.
+//    |left subtree height - right subtree height| <= 1.
+//These two ways will use bottom-up approach meaning thay will go to the leaf node and come up eith the result.
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -14,7 +18,7 @@ class Node{
         }
 };
 
-int height(Node* root){
+int height(Node* root){//O(n)
     if(root == NULL){
         return 0;
     }
@@ -23,6 +27,8 @@ int height(Node* root){
     
     return max(lheight, rheight) + 1;
 }
+//As these function will go to each node, the time complexity will be O(n). The height function will also go to each node so the time 
+//complexitty will also be O(n). Total time complexity will be O(n^2).
 bool balancedtree(Node* root){
     if(root == NULL){
         return true;
@@ -75,10 +81,12 @@ int main(){
 .........................................................................................
 .........................................................................................
 .........................................................................................
+.................OPTIMIZED WAY OF CHECKING IF A TREE IS BALANCED OR NOT..................
+//O(n) n---> number of nodes.
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node{
+class Node{//Basic structure of a Node.
     public:
         int data;
         Node* left;
@@ -91,19 +99,22 @@ class Node{
         }
 };
 
-bool balancedtree(Node* root, int* height){
-    if(root == NULL){
+bool balancedtree(Node* root, int* height){//Will return a boolean value, will take arguments as root and height.
+    if(root == NULL){//If there are no roots meaning that the tree is empty.
         return true;
     }
-    int lh = 0, rh = 0;
-    if(balancedtree(root->left, &lh) == false){
+    int lh = 0, rh = 0;//Intializing left and right height of subtree.
+    if(balancedtree(root->left, &lh) == false){//Checking for left subtree.
         return false;
     }
-    if(balancedtree(root->right, &rh) == false){
+    if(balancedtree(root->right, &rh) == false){//Checking for right subtree.
         return false;
     }
-    *height = max(lh, rh) + 1;
-    if(abs(lh - rh) <= 1){
+    *height = max(lh, rh) + 1;//To calculate the height of a tree we take the 'max' of height of its Left subtree and right subtree.
+                              //And add 1 to it.
+                              //If we were to start from the bottom most node, its height will 1 becuase it doesn't have any leaf nodes,
+                              //so we will just add a '1' for itself.
+    if(abs(lh - rh) <= 1){//Take the absolute value meaning only positive value.
         return true;
     }
     else{
