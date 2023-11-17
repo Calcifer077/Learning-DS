5a5ac9048e46aa9ed3f07adfbbe96651496d6449
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-11-17 22:21:49 +0530

    Update Basics.java

--- Java/AWT/Basics.java
+++ Java/AWT/Basics.java
@@ -1,6 +1,10 @@
 // Java AWT Program for Hello World 
 import java.awt.*; 
 import java.awt.event.WindowAdapter; 
+//An abstract adapter class for receiving window events. Window events like closing a window, resizing it, opening etc.
+//The methods in this class are empty. 
+//This class exists as convenience for creating listener objects. 
+//Extend this class to create a WindowEvent listener and override the methods for the events of interest.
 import java.awt.event.WindowEvent; 
 
 // Driver Class 
@@ -24,10 +28,14 @@ public class Main {
 		frame.setVisible(true); 
 
 		// Using WindowListener for closing the window 
+		//The below line adds 'WindowListener' to the frame. It listenes to the event related to a window.
 		frame.addWindowListener(new WindowAdapter() { //This will make the new window close when the close button is pressed.
 			@Override
-			public void windowClosing(WindowEvent e) 
-			{ 
+			// This method is overridden from the WindowListener interface. 
+			//It specifies what should happen when the window is closing.
+			public void windowClosing(WindowEvent e) { 
+				//This line calls the exit method of the System class, terminating the Java Virtual Machine (JVM) with a status code of 0. 
+				//In this context, it means that the application will exit when the window is closed.
 				System.exit(0); 
 			} 
 		}); 
