b6cf1825e0ccd366507211cd88f0c374f5f1b950
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-11-18 13:11:15 +0530

    Update Basics.java

--- Java/GUI/Swing/Basics.java
+++ Java/GUI/Swing/Basics.java
@@ -13,3 +13,20 @@ class Main{
 		f.setVisible(true);
 	}
 }
+.........................................................................................
+.........................................................................................
+.........................................................................................
+import javax.swing.*;
+
+class Main{
+	public static void main(String[] args) {
+		JFrame f = new JFrame();//an instance of JFrame
+		JButton b = new JButton("Click here");
+
+		b.setBounds(150, 200, 220, 50);//x, y width, height
+		f.add(b);
+		f.setSize(500, 600);//width and height
+		f.setLayout(null);//setting layout manager as 'null', meaning not using any.
+		f.setVisible(true);//Making the frame visible.
+	}
+}
