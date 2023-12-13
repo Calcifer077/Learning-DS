e6b417ab49a747b4c9674135174b5bb6ab116a68
Calcifer077 <115633814+Calcifer077@users.noreply.github.com>
2023-12-13 19:31:37 +0530

    Update Insertion.java

--- Java/LINKED LIST/Insertion.java
+++ Java/LINKED LIST/Insertion.java
@@ -1,10 +1,10 @@
 public class Linkedlist{
         Node head;
-        static class Node{
+        static class Node{//Basic structure of a node
             int data;
             Node next;
 
-            Node(int val){
+            Node(int val){//Constructor of class node
                 data = val;
                 next = null;
             }
@@ -12,26 +12,27 @@ public class Linkedlist{
         public static Linkedlist insert(Linkedlist list, int data){
             Node new_Node = new Node(data);//creating a new node with the given value
 
-            if(list.head == null){
+            if(list.head == null){//If there are no elements in the linked list. 
                 list.head = new_Node;
             }
             else{
-                Node temp = list.head;
-                while(temp.next != null){
-                    temp = temp.next;
+                Node temp = list.head;//Creating a temporary variable that is used to iterate in the linked list. 
+                while(temp.next != null){//Iterating until it reaches the last node
+                    temp = temp.next;//Iteration 
                 }
-                temp.next = new_Node;
+                temp.next = new_Node;//Inserting the new node at the last or can say in the next of temp. 
             }
-            return list;
+            return list;//Returning the final linked list
         }
 
-        public static void printlist(Linkedlist list){
-            Node temp = list.head;
+        public static void printlist(Linkedlist list){//Displaying the end result
+            Node temp = list.head;//Using a temporary variable for iteration
+         
             System.out.println("Linkedlist");
 
-            while(temp != null){
+            while(temp != null){//Do it until temp is at null
                 System.out.print(temp.data + "->");
-                temp = temp.next;
+                temp = temp.next;//Iteration
             }
             System.out.print("Null");
         }
