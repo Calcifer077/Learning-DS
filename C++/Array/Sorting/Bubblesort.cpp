fa5ad08498752693c03267e283520ed36b0ca0ea
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-05-30 10:30:44 +0530

    Update Bubblesort.cpp

--- C++/Array/Sorting/Bubblesort.cpp
+++ C++/Array/Sorting/Bubblesort.cpp
@@ -27,3 +27,43 @@ int main(){
 	}
 	return 0;
 }
+.........................................................................
+.........................................................................	
+.........................................................................
+
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size){
+    int i, j;
+    bool swaped;
+    
+    for(i = 0; i < size - 1; i++){
+        swaped = false;
+        for(j = 0; j < size - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swaped = true;
+            }
+        }
+        if(swaped == false){
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size){
+    for(int i = 0 ; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int arr[] = {7,3,8,1,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, size);
+    printArray(arr, size);
+    
+    return 0;
+}
