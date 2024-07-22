b67a5334dd81f7925840bdb629144c2998ad1c62
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-22 23:15:32 +0530

    Create decimal to binary.java

---
+++ Java/BASICS/decimal to binary.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static double decimalToBinary(int n){
+        double result = 0;
+        int pow = 0;
+        while(n > 0){
+            int rem = n % 2;
+            n = n/2;
+            result = result + (rem * (Math.pow(10, pow)));
+            pow++;
+        }
+        return result;
+    }
+    public static void main (String[] args) {
+        System.out.println(decimalToBinary(7));
+    }
+}
