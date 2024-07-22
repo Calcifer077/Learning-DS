bd383461e635661a95370ef271e8e9a0dc237563
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-22 22:09:23 +0530

    Update and rename prime number.java to isPrime.java

---
+++ Java/BASICS/isPrime.java
@@ -0,0 +1,32 @@
+public class Main
+{   
+    // public static boolean isPrime(int n){
+    //     if(n == 2){
+    //         return true;
+    //     }
+    //     for(int i = 2; i < n; i++){
+    //         if(n % i == 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // Below approach is more optimized than the above one.
+    public static boolean isPrime(int n){
+        if(n == 2){
+            return true;
+        }
+        for(int i = 2; i < Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+	public static void main(String[] args) {
+		System.out.println(isPrime(50));
+	}
+}
