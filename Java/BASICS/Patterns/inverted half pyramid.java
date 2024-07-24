fd33a6978067138857fecd3a67266a493dc21d85
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-24 19:58:49 +0530

    Update inverted half pyramid.java

--- Java/BASICS/Patterns/inverted half pyramid.java
+++ Java/BASICS/Patterns/inverted half pyramid.java
@@ -1,5 +1,11 @@
 public class Main
 {
+	// 12345
+	// 1234
+	// 123
+	// 12
+	// 1
+	
     public static void InvertedHalfPyramid(int n){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= (n+1-i); j++){
