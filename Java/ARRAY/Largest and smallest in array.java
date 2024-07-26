e3825392f5e471ae524053be3b192365e2de8d7b
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-26 11:32:10 +0530

    Create Largest and smallest in array.java

---
+++ Java/ARRAY/Largest and smallest in array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+	public static void largestAndSmallest(int arr[]){
+      // Both below values comes from the 'util' package.
+	    int largest = Integer.MIN_VALUE; // - Infinity
+	    int smallest = Integer.MAX_VALUE; // +Infinity
+	    for(int i = 0; i < arr.length; i++){
+	        if(arr[i] > largest){
+	            largest = arr[i];
+	        }
+	        if(arr[i] < smallest){
+	            smallest = arr[i];
+	        }
+	    }
+	    System.out.println(largest + " , " + smallest);
+	}
+  
+	public static void main (String[] args) {
+		int arr[] = {2, 5, 9, 10, 1, 3, 14};
+		largestAndSmallest(arr);
+	}
+}
