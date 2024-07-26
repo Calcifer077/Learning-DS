db5709238f63b7a3496d852f75910b40b6adb970
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-26 11:27:08 +0530

    Create Linear search.java

---
+++ Java/ARRAY/Searching/Linear search.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static int linearSearch(int arr[], int key){
+	    for(int i = 0; i < arr.length; i++){
+	        if(arr[i] == key){
+	            return (i+1);
+	        }
+	    }
+	    return -1;
+	}
+	public static void main (String[] args) {
+		int arr[] = {2, 5, 9, 10, 1, 3, 14};
+		System.out.println(linearSearch(arr, 10));
+	}
+}
