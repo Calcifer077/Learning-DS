60bea1b6b083a9fc2a8ed45bbb01a44653710d85
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-26 11:36:42 +0530

    Create pairs.java

---
+++ Java/ARRAY/pairs.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void pairs(int arr[]){
+	    for(int i = 0; i < arr.length; i++){
+	        for(int j = i + 1; j < arr.length; j++){
+	            System.out.print("( " + arr[i] + " " + arr[j] + " ) , ");
+	        }
+	    }
+	}
+
+	public static void main (String[] args) {
+		int arr[] = {2, 5, 9, 10, 1, 3, 14};
+		pairs(arr);
+	}
+}
