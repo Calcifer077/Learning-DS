60bb1099f8307f2d6a7e89fea549bfcc6d126a85
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-30 22:51:56 +0530

    Create find shortest path.java

---
+++ Java/String/find shortest path.java
@@ -0,0 +1,23 @@
+public class Main{
+    
+    public static int findShortestPath(String str){
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'N'){
+                y++;
+            }else if(str.charAt(i) == 'S'){
+                y--;
+            }else if(str.charAt(i) == 'E'){
+                x++;
+            }else{
+                x--;
+            }
+        }
+        int res = (int)Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+        return res;
+    }
+    public static void main (String[] args) {
+       System.out.println(findShortestPath("WNEENWSWNNN"));
+    }
+}
