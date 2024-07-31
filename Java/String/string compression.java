67857f932d59b96040a70ec6264e32505b009ae1
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-07-31 22:40:46 +0530

    Create string compression.java

---
+++ Java/String/string compression.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main{
+    
+    public static String stringCompression(String str){
+        StringBuilder sb = new StringBuilder("");
+        
+        Integer count;
+        for(int i = 0; i < str.length(); i++){
+            count = 1;
+            while(i < str.length() - 1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            if(count > 1){
+                sb.append(str.charAt(i));
+                sb.append(count);
+            }else{
+                sb.append(str.charAt(i));
+            }
+            
+        }
+        
+        return sb.toString();
+    }
+    public static void main (String[] args) {
+        String str = "aaabbcccdd";
+        System.out.println(stringCompression(str));
+    }
+}
