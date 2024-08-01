f31b2c5132aaa71e4dd7a017ceee419df84f74a6
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-08-01 22:10:43 +0530

    Create check odd or even.java

---
+++ Java/Bit Manipulation/check odd or even.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    // public static void checkEvenOdd(int num){
+    //     num = num & 1;
+    //     if(num == 1){
+    //         System.out.println("Odd");
+    //     }else{
+    //         System.out.println("Even");
+    //     }
+    // }
+
+    public static void checkEvenOdd(int num){
+        int bitmask = 1;
+        if((num & bitmask) == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+	public static void main(String[] args) {
+		checkEvenOdd(11);
+	}
+}
