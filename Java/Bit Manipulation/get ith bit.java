841be627b466ea5659e738974900f6c1ee04628e
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-08-01 22:14:33 +0530

    Create get ith bit.java

---
+++ Java/Bit Manipulation/get ith bit.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static int getIthBit(int num, int i){
+        int bitmask = 1 << i;
+        if((num & bitmask) != 1){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+	public static void main(String[] args) {
+		System.out.println(getIthBit(15, 2));
+	}
+}
