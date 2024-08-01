824dd11c08c61d09d5a238b38981e0b4da776c8b
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2024-08-01 22:20:35 +0530

    Create set ith bit.java

---
+++ Java/Bit Manipulation/set ith bit.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static int setIthBit(int num, int i){
+        // In this we have to set(convert to 1) ith bit.
+        int bitmask = 1 << i;
+        return num | bitmask;
+    }
+	public static void main(String[] args) {
+		System.out.println(setIthBit(10, 2));
+	}
+}
