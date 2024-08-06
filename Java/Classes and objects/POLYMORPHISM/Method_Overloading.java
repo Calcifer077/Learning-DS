4983a74d0882dc2c8c60d1fc7194f8c8fae5c4a7
Calcifer077 <maheshnashier14@gmail.com>
2024-08-06 21:48:18 +0530

    Wrote about polymorphism

--- Java/Classes and objects/POLYMORPHISM/Method_Overloading.java
+++ Java/Classes and objects/POLYMORPHISM/Method_Overloading.java
@@ -1,25 +1,22 @@
-//Polymorphism
-import java.util.Scanner;
-class Base{
-    void area(){
-        System.out.println("No arguments passed");
-    }
-    void area(int a, int b){
-        System.out.println("The area of the gieven rectangle is: " + (a * b));
-    }
-    void area(double a, double b){
-        System.out.println("The area of the given rectangle is: " + (a + b));
+public class method_overloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(1, 2));
+        System.out.println(calc.sum(1.0, 2.0));
+        System.out.println(calc.sum(1, 2, 3));
     }
 }
 
-class Main{
-    public static void main(String[] args){
-        Base obj = new Base();
-        obj.area();
-        obj.area(2,3);
-        obj.area(5.0, 4.8);
+class Calculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    double sum(double a, double b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
-.........................................................................................
-.........................................................................................
-.........................................................................................
--- Java/Classes and objects/POLYMORPHISM/Method_Overriding.java
+++ Java/Classes and objects/POLYMORPHISM/Method_Overriding.java
@@ -1,27 +1,18 @@
-class Base{
-    void print(){
-        System.out.println("This is the Base class.");
-    }
-}
-
-class Derived1 extends Base{
-    void print(){
-        System.out.println("The is the derived1 class.");
-    }
-}
-
-class Derived2 extends Base{
-    void print(){
-        System.out.println("The is the derived2 class.");
-    }
-}
-
-public class Main{
+public class method_overriding {
     public static void main(String[] args) {
-        Base obj1;
-        obj1 = new Derived1();
-        obj1.print();
-        obj1 = new Derived2();
-        obj1.print();
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Eats anything");
+    }
+}
+
+class Deer extends Animal {
+    void eat() {
+        System.out.println("eats grass");
     }
 }
